f626435fe70ae25a0552d81fbaf5c4209bb5fca5
Skine <55518255+bskine@users.noreply.github.com>
2020-09-22 07:47:11 -0800

    Update ruckus_alarms.py

--- ruckus_alarms.py
+++ ruckus_alarms.py
@@ -2,14 +2,27 @@ import requests
 from pprint import *
 import datetime
 
-url = "https://136.226.130.97:8443/wsg/api/public/v8_2/alert/alarm/list?serviceTicket=ST-80-pb29SiHzCpOf3QiejVTV-Ruckus-One"
+url_init = "https://136.226.130.97:8443/wsg/api/public/v8_2/serviceTicket"
+
+payload = "{\r\n  \"username\": \"pboynto\",\r\n  \"password\": \"Alpine4911!\"\r\n}"
+headers = {
+  'Authorization': 'Basic cGJveW50bzpBbHBpbmU0OTExIQ==',
+  'Content-Type': 'text/plain',
+  'Cookie': 'JSESSIONID=7AD94AB1886B4C505459FF72DB5ADA50'}
+
+reply = requests.request("POST", url_init, headers=headers, data = payload, verify=False)
+r = reply.json()
+
+service_ticket = r['serviceTicket']
+
+
+url = ("https://136.226.130.97:8443/wsg/api/public/v8_2/alert/alarm/list?serviceTicket="+ service_ticket)
 
 payload = "{\r\n  \"filters\": [\r\n    {\r\n      \"type\": \"ZONE\",\r\n      \"value\": \"e7c5e1cf-abc2-445c-abc8-ba0a6d0982f8\",\r\n      \"operator\": \"eq\"\r\n    }\r\n  ]\r\n} "
 headers = {
   'Content-Type': '[application/json;charset=UTF-8',
   'Authorization': 'Basic cGJveW50bzpBbHBpbmU0OTExIQ==',
-  'Cookie': 'JSESSIONID=882B3E8477E1730E7439B2BF6066FFFE'
-}
+  'Cookie': 'JSESSIONID=882B3E8477E1730E7439B2BF6066FFFE'}
 
 response = requests.request("POST", url, headers=headers, data = payload, verify=False)
 x = response.json()
